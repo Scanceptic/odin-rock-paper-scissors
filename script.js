be0ba0a4a04bb5a16ea9f83073b139b8bf77c9a3
Nathan <urwin.nathan@gmail.com>
2024-06-18 13:23:07 +0100

    Made initial rps computer generator

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+const getComputerChoice = function() {
+    let choiceVal = Math.random() * 3;
+    console.log("Random Number is " + choiceVal);
+    choiceVal = Math.floor(choiceVal);
+    console.log("Rounded number is " + choiceVal);
+    if (choiceVal === 0) {
+        return "Rock";
+    } else if (choiceVal === 1) {
+        return "Paper";
+    } else if (choiceVal === 2) {
+        return "Scissors";
+    } else {
+        return "Error";
+    }
+}
+
+console.log(getComputerChoice());
