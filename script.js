eb9bd3e89d7f09d70cfdf852ed0763487de187b6
Nathan <urwin.nathan@gmail.com>
2024-06-23 15:45:22 +0100

    Add button styling

--- script.js
+++ script.js
@@ -37,8 +37,7 @@ const playRound = function(humanChoice, computerChoice) {
                     break;
                 case "Scissors":
                     //console.log("Scissors");
-                    result.textContent = "You Win! Rock beats Scissors!";
-                    results.appendChild(result);
+                    results.appendChild(createResult("You Win! Rock beats Scissors!"));
                     humanScore++;
                     break;
             }
@@ -46,17 +45,14 @@ const playRound = function(humanChoice, computerChoice) {
         case "Paper":
             switch(computerChoice) {
                 case "Rock":
-                    console.log("You Win! Paper beats Rock!");
-                    results.appendChild(result);
+                    results.appendChild(createResult("You Win! Paper beats Rock!"));
                     humanScore++;
                     break;
                 case "Paper":
-                    console.log("It's a Draw! Paper can't beat Paper!");
-                    results.appendChild(result);
+                    results.appendChild(createResult("It's a Draw! Paper can't beat Paper!"));
                     break;
                 case "Scissors":
-                    console.log("You Lose! Scissors beat Paper!");
-                    results.appendChild(result);
+                    results.appendChild(createResult("You Lose! Scissors beat Paper!"));
                     computerScore++;
                     break;
             }
@@ -64,18 +60,15 @@ const playRound = function(humanChoice, computerChoice) {
         case "Scissors":
             switch(computerChoice) {
                 case "Rock":
-                    console.log("You Lose! Rock beats Scissors!");
-                    results.appendChild(result);
+                    results.appendChild(createResult("You Lose! Rock beats Scissors!"));
                     computerScore++;
                     break;
                 case "Paper":
-                    console.log("You Win! Scissors beat Paper!");
-                    results.appendChild(result);
+                    results.appendChild(createResult("You Win! Scissors beat Paper!"));
                     humanScore++;
                     break;
                 case "Scissors":
-                    console.log("It's a Draw! Scissors can't beat Scissors!");
-                    results.appendChild(result);
+                    results.appendChild(createResult("It's a Draw! Scissors can't beat Scissors!"));
                     break;
             }
         break;
@@ -89,17 +82,14 @@ const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
 rock.addEventListener("click", () => {
-    //console.log("Human Played Rock");
     playRound("Rock", getComputerChoice());
 });
 
 paper.addEventListener("click", () => {
-    //console.log("Human Played Paper");
     playRound("Paper", getComputerChoice());
 });
 
 scissors.addEventListener("click", () => {
-    //console.log("Human Played Scissors");
     playRound("Scissors", getComputerChoice());
 });
 
